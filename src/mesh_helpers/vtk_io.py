a90d5d74845124d194e1934b62e9b5b21c78a813
jilei-hao <jilei.cs@gmail.com>
2024-09-25 17:11:27 -0400

    initial commit

---
+++ src/mesh_helpers/vtk_io.py
@@ -0,0 +1,42 @@
+import vtk
+
+def read_polydata(filename):
+  # if filename ends iwth .vtk, use vtkPolyDataReader
+  if filename.endswith('.vtk'):
+    reader = vtk.vtkPolyDataReader()
+  # if filename ends iwth .vtp, use vtkXMLPolyDataReader
+  elif filename.endswith('.vtp'):
+    reader = vtk.vtkXMLPolyDataReader()
+  # if filename ends iwth .stl, use vtkSTLReader
+  elif filename.endswith('.stl'):
+    reader = vtk.vtkSTLReader()
+  # if filename ends iwth .obj, use vtkOBJReader
+  elif filename.endswith('.obj'):
+    reader = vtk.vtkOBJReader()
+  else:
+    raise ValueError('Unknown file format')
+  
+  reader.SetFileName(filename)
+  reader.Update()
+  return reader.GetOutput()
+
+def write_polydata(polydata, filename):
+  # if filename ends iwth .vtk, use vtkPolyDataWriter
+  if filename.endswith('.vtk'):
+    writer = vtk.vtkPolyDataWriter()
+  # if filename ends iwth .vtp, use vtkXMLPolyDataWriter
+  elif filename.endswith('.vtp'):
+    writer = vtk.vtkXMLPolyDataWriter()
+  # if filename ends iwth .stl, use vtkSTLWriter
+  elif filename.endswith('.stl'):
+    writer = vtk.vtkSTLWriter()
+    writer.SetFileTypeToBinary()
+  # if filename ends iwth .obj, use vtkOBJWriter
+  elif filename.endswith('.obj'):
+    writer = vtk.vtkOBJWriter()
+  else:
+    raise ValueError('Unknown file format')
+  
+  writer.SetInputData(polydata)
+  writer.SetFileName(filename)
+  writer.Write()
